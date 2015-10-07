a5828f4a7ee41ee9ab7c9edfa28fb490cb93e0f9
gitlouie <louietorres93@gmail.com>
2015-10-06 20:51:17 -0700

    Added the functionality to the button. The start button goes to the next scene where the game will begin. Added some classes to generalize the code to make it easier to manage.

---
+++ Eat em UP remote/Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+
+    //Changes the scene to the next 
+	public void ChangeToScene (string sceneToChangeTo) {
+        Application.LoadLevel(sceneToChangeTo);
+	}
+}
---
+++ Eat em UP remote/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private float health;
+    private string name;
+
+    //initalize the player based on the inputs
+    public Player(int health, string name)
+    {
+        this.health = health;
+        this.name = name;
+
+    }
+
+    public float getHealth()
+    {
+        return health;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+}
