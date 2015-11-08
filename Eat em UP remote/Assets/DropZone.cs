f3000988da5633eebe80b395c6f0a3d7a74d31b0
gitlouie <louietorres93@gmail.com>
2015-11-07 17:02:09 -0800

    Changed the scene to the actual game window...Cards are no displayed and can be moved

---
+++ Eat em UP remote/Assets/DropZone.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Draggable.Slot typeOfItem = Draggable.Slot.EQUIP;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (typeOfItem == d.typeOfItem) d.placeholderParent = this.transform;
+        }
+    }
+	
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            if (typeOfItem == d.typeOfItem) d.placeholderParent = d.parentToReturnTo;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d!= null)
+        {
+            if(typeOfItem == d.typeOfItem) d.parentToReturnTo = this.transform;
+        }
+
+    }
+
+}
